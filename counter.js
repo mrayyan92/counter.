5b488973b8eb4dce71fa582d25287f873dc0d17c
mrayyan92 <shaheenkhurram0@gmail.com>
2025-05-28 13:09:45 +0500

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+    let count = 0;
+  
+    const countDisplay = document.getElementById('count');
+    const decreaseBtn = document.getElementById('decrease');
+    const resetBtn = document.getElementById('reset');
+    const increaseBtn = document.getElementById('increase');
+  
+    decreaseBtn.addEventListener('click', () => {
+      count--;
+      updateDisplay();
+    });
+  
+    resetBtn.addEventListener('click', () => {
+      count = 0;
+      updateDisplay();
+    });
+  
+    increaseBtn.addEventListener('click', () => {
+      count++;
+      updateDisplay();
+    });
+  
+    function updateDisplay() {
+      countDisplay.textContent = count;
+    }
+  };
+  
+ 
+  
+  Sources
+  
+  
+  
+  
+  
+  
+  
+  
+  
